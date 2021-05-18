1680c544addb340892d0a479b03fb6f515df46f8
Dani_PG <dapega01@gmail.com>
2021-05-18 16:31:37 +0200

    redireccion a lista juegos desde juegos

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ app = Flask(__name__)
 with open("MSX.json") as fichero:
         doc = json.load(fichero)
 
-@app.route('/')
+@app.route('/',methods=["GET"])
 def pagina_principal():
     return render_template("paginaprincipal.html",documento=doc)
 
-@app.route('/juegos',methods=["GET"])
+@app.route('/juegos',methods=["GET","POST"])
 def pagina_juegos():
-        juego=request.form.get("juego")
-        return render_template("juegos.html", juego=juego)
+        if request.method=="GET":
+                juego=request.form.get("juego")
+                return render_template("juegos.html", juego=juego)
+        else:
+                return render_template("listajuegos.html")
 
 @app.route('/listajuegos')
 def pagina_lista_juegos():
